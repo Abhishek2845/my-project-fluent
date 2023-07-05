a613f47ef84aae4c9d3f8dd2c4eebe5356982683
Abhishek Yadav <abhishek99opyadav@gmail.com>
2023-07-05 23:35:39 +0530

    First commit

---
+++ assets/js/search.bar.js
@@ -0,0 +1,12 @@
+let inputBox = document.querySelector('.input-box'),
+  searchIcon = document.querySelector('.search'),
+  closeIcon = document.querySelector('.close-icon');
+
+
+searchIcon.addEventListener('click', () => {
+  inputBox.classList.add('open');
+});
+
+closeIcon.addEventListener('click', () => {
+  inputBox.classList.remove('open');
+});
